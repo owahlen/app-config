c7dde719619c4915a6c6747f9ea6c836801f9e10
Oliver Wahlen <oliver.wahlen@infinit-group.de>
2024-10-11 12:29:21 +0200

    added validation script

---
+++ validate.js
@@ -0,0 +1,18 @@
+const Ajv = require('ajv');
+const fs = require('fs');
+
+// Load JSON and Schema
+const data = JSON.parse(fs.readFileSync('app-config.json', 'utf-8'));
+const schema = JSON.parse(fs.readFileSync('app-config-schema.json', 'utf-8'));
+
+// Initialize AJV
+const ajv = new Ajv();
+const validate = ajv.compile(schema);
+const valid = validate(data);
+
+// Perform validation
+if (!valid) {
+  console.log("Invalid JSON!");
+  console.log(validate.errors); // Log detailed validation errors
+  process.exit(1); // Failure exit code
+}
